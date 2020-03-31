9daf7477bf07f0dc6331220e3490f5778ddbbd82
SeongIkKim <kpic1638@gmail.com>
2020-03-31 17:02:35 +0900

    [UPDATE] week_4 lecture fetch.js annotation

--- week_4(03.28)/fetch.js
+++ week_4(03.28)/fetch.js
@@ -1,5 +1,7 @@
-fetch("https://api.hnpwa.com/v0/news/1.json")
-  .then(response => {
+fetch("https://api.hnpwa.com/v0/news/1.json") // 이 fetch는 URL주소에 요청한 결과를 바로 반환하지 않는다. 대신에 요청한 결과를 가져올 promise객체(일반 HTTP Response)를 반환한다.
+  .then(response => { // 이 시점의 then은 promise 객체를 가져오는것을 완료(resolved)하였을때 진행된다.
+    // 그러나 promise 객체는 응답을 아직 받아오지 못했으므로(async 상태), promise의 상태는 pending(대기중)으로 나온다.
+    console.log(response.json());
     return response.json();
   })
   .then(response => {
@@ -10,3 +12,4 @@ fetch("https://api.hnpwa.com/v0/news/1.json")
   .catch(error => {
     console.log(error);
   });
+console.log("프로그램 종료");
