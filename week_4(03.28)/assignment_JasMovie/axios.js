e16673b083f84f6fe81f362be4bae6a860dfad7b
SeongIkKim <kpic1638@gmail.com>
2020-04-04 01:46:39 +0900

    [CREATE] week4 assignment_jasMovie list page

--- week_4(03.28)/assignment_JasMovie/axios.js
+++ week_4(03.28)/assignment_JasMovie/axios.js
@@ -1,16 +1,82 @@
-const example = async () => {
+// const movieCard = document.querySelector(".movie-card"),
+//     poster = movieCard.querySelector(".poster"),
+//     card-body = movieCard.querySelector("")
+
+const app = document.querySelector("#app");
+
+const makeMovieCard = (movieDetail) => {
+    const movieCard = document.createElement("div")
+    movieCard.classList.add("movie-card");
+
+    const poster = document.createElement("img");
+    poster.classList.add("poster");
+    poster.src = movieDetail.large_cover_image;
+
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    
+    const h1 = document.createElement("h1");
+    const div = document.createElement("div");
+    const span = document.createElement("span");
+    const p = document.createElement("p");
+
+    h1.innerText = movieDetail.title;
+    movieDetail.genres.forEach(function(genre){
+        const pForGenre = document.createElement("p");
+        pForGenre.innerText = genre; 
+        div.append(pForGenre);
+    });
+    p.innerText = movieDetail.description_intro;
+
+    cardBody.appendChild(h1);
+    cardBody.appendChild(div);
+    cardBody.appendChild(p);
+
+    movieCard.appendChild(poster);
+    movieCard.appendChild(cardBody);
+
+    return movieCard;
+}
+
+
+const mainInit = async () => {
     try {
-      const response = await axios.get(
-        //"https://yts.mx/api/v2/movie_suggestions.json?movie_id=10"
-        "https://yts.mx/api/v2/list_movies.json?limit=30&minimum_rating=5&genre=Drama&sorted_by=like_count&with_rt_ratings=true"
-      );
-      console.log(response.data);
-  
-      const movies = response.data.data.movies;
-      console.log(movies);
+        var response = await axios.get(
+        "https://yts.mx/api/v2/list_movies.json?limit=16&minimum_rating=5&genre=Drama&sorted_by=like_count&with_rt_ratings=true"
+        );
+        // console.log(response.data);
+        
+        // 1페이지 movie 16개 받아옴
+        // 최소 5점이상, 드라마장르, 좋아요 갯수순, 로튼토마토 점수까지
+        const movieList = response.data.data.movies;
+        console.log(movieList);
+        
+        // 각 movie별로 card 만들고 정보 채워넣어 띄우기
+        for(var i=0; i<movieList.length; i++){
+            response = await axios.get(
+                `https://yts.mx/api/v2/movie_details.json?movie_id=${movieList[i].id}&with_images=true`
+            );
+            const movieDetail = response.data.data.movie;
+            // 최초의 movie로 만든 moviecard app에 추가
+            const movieCard = makeMovieCard(movieDetail);
+            app.appendChild(movieCard);
+        }
+
+
+
+
+        // 첫번째 movie의 추천영화
+        // 추후 for문으로 대체
+        // response = await axios.get(
+        //     `https://yts.mx/api/v2/movie_suggestions.json?movie_id=${movieList[0].id}`
+        // )
+        // const movieSuggestion = response.data.data.movies;
+        // console.log("moive suggestion은");
+        // console.log(movieSuggestion);
+        
     } catch (error) {
-      console.log(error);
+        console.log(error);
     }
-  };
-  
-  example();
+};
+
+mainInit();
