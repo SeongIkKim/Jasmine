c719547513ff0a20538e9bfd8852278a9a50eef8
SeongIkKim <kpic1638@gmail.com>
2020-05-17 02:03:36 +0900

    [UPDATE] Node_Basic Section 3 - Database 연동 기본

--- Node_basic/app.js
+++ Node_basic/app.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host : 'localhost',
+    port : 3306,
+    user : 'root',
+    password : 'asdf1234',
+    database : 'basic'
+});
+
+connection.connect();
 
 app.listen(3000, function() {
     console.log("start! express server on port 3000");
@@ -16,6 +27,7 @@ app.set('view engine','ejs');
 app.get('/main', function(req,res){
     res.sendFile(__dirname + "/public/main.html");
 });
+
 app.post('/email_post', function(req,res){
     //get : req.param("email");
     console.log(req.body.email); // 객체가 서버로 전달되어 로그로 찍힌다.
@@ -24,9 +36,22 @@ app.post('/email_post', function(req,res){
 });
 
 app.post('/ajax_send_email', function(req,res){ 
-    /* 3. server에서 데이터 처리 후 다시 html로 반환 */
-    console.log(req.body.email); // ajax로 받은 json 객체의 email 값이 서버에 로그로 찍힌다.
-    // check validation about input data => select DB // 받은 데이터가 올바른 값인지 확인하고, DB에 정보를 넣거나 빼오는 과정이 여기에 들어간다.
-    var responseData = {'result' : 'ok', 'email' : req.body.email};
-    res.json(responseData); // 가공해서 만든 json형태의 responsedata를 html파일로 다시 넘긴다.
+    var email = req.body.email;
+    var responseData = {};
+
+    var query = connection.query('select name from user where email="' + email +'"', function(err, rows){ // mysql 쿼리처리
+        if(err) throw err; // 에러처리
+        if(rows[0]){
+            responseData.result = "ok";
+            responseData.name = rows[0].name; // 반환된 json row data에서 name값을 뽑아낸다
+        } else {
+            // 해당 email을 가진 user가 존재하지 않을때
+            responseData.result = "none"; 
+            responseData.name = "";
+        }
+        res.json(responseData);
+        /* response 값을 주는 위의 코드는 반드시 콜백함수 내에 존재해야 한다.
+        만약 콜백함수 바깥에 존재한다면 비동기로 처리되어 mysql에서 데이터를 불러오기도 전에 수행되고,
+        결국 값을 제대로 불러오지 못하게 된다. */
+    })
 })
