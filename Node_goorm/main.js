0a28748568f9e7b870f7aea6dfc7d8b5c4cf9a5c
SeongIkKim <kpic1638@gmail.com>
2020-07-11 15:58:21 +0900

    [PRACTICE] using require

--- Node_goorm/main.js
+++ Node_goorm/main.js
@@ -1,11 +1,3 @@
-var EventEmitter = require("events").EventEmitter;
-var evt = new EventEmitter();
-
-evt.on("helloNode", function(str){
-    console.log("Hello!" + str);
-});
-
-// setTimeout은 두번째 인자만큼의 milisecond 이후에 특정 함수를 콜백으로 실행시킨다.
-setTimeout(function(){
-    evt.emit("helloNode", "Node.js");
-}, 3000);
+var caculator = require('./test.js'); //python에서 import처럼 쓰는 개념
+console.log('Double value is '+ caculatordouble(3));
+console.log('Plus value is '+ calculator.plus(3));
